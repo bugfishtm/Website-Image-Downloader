92ebc808781b6d883a22d91bc1123afcd52748db
Jan-Maurice Dahlmanns <27702527+bugfishtm@users.noreply.github.com>
2020-12-19 07:11:22 +0100

    Add files via upload

---
+++ MainClass.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace wib
+{
+    class ImageDownloader
+    {
+        ////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////
+        /// Reset Search Vars
+        ////////////////////////////////////////////////////
+            string contextcode = null;
+            public void ResetVars() { this.contextcode = null;  }
+            public bool getContext() { if (this.contextcode.Equals(null)) { return false; } return true; }
+
+        ////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////
+        /// Get a Complete HTML Website as String
+        ////////////////////////////////////////////////////
+            public void getHtmlUrlAsString(String URL)
+            {
+                if (URL == null) { return; }
+                HttpWebRequest request;
+                HttpWebResponse response;
+                    try
+                    {
+                        request = (HttpWebRequest)WebRequest.Create(URL);
+                        response = (HttpWebResponse)request.GetResponse();
+                    }
+                    catch (WebException) { return; }
+                    catch (Exception) { return; }
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream;
+                    if (String.IsNullOrWhiteSpace(response.CharacterSet))
+                        readStream = new StreamReader(receiveStream);
+                    else
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    this.contextcode = readStream.ReadToEnd();
+                    response.Close();
+                    readStream.Close();
+                    return;
+                }
+
+                response.Close();
+                return;
+            }
+
+        ////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////
+        /// Download Images If Not Exists
+        ////////////////////////////////////////////////////
+            public bool downloadAnImage(string filename, string imageUrl)
+            {
+                string[] tmpformatstring = imageUrl.Split('.');
+                ImageFormat format;
+
+            switch (tmpformatstring[tmpformatstring.Length - 1])
+            {
+                case "jpg": case "JPG": format = ImageFormat.Jpeg; break;
+                case "png": case "PNG": format = ImageFormat.Png; break;
+                case "jpeg": case "JPEG": format = ImageFormat.Jpeg; break;
+                case "gif": case "GIF": format = ImageFormat.Gif; break;
+                case "bmp": case "BMP": format = ImageFormat.Bmp; break;
+                case "tiff": case "TIFF": format = ImageFormat.Tiff; break;
+                case "ico": case "ICO": format = ImageFormat.Icon; break;
+                default: return false;
+            };
+
+            if (imageUrl.Substring(0, 4) != "http") { return false; }
+
+                if (File.Exists(filename + "." + tmpformatstring[tmpformatstring.Length - 1])) { return true; }
+                WebClient client = new WebClient();
+                Stream stream = client.OpenRead(imageUrl);
+                Bitmap bitmap; bitmap = new Bitmap(stream);
+
+                if (bitmap != null) { 
+                    bitmap.Save(filename + "." + tmpformatstring[tmpformatstring.Length - 1], format);
+                    stream.Flush();
+                    stream.Close();
+                    client.Dispose();
+                    return true; }
+                else {
+                    stream.Flush();
+                    stream.Close();
+                    client.Dispose();
+                    return false; }
+            }
+
+        ////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////
+        /// GET IMAGE LINKS FROM CONTEXT
+        ////////////////////////////////////////////////////
+        public List<string> getImageLinks()
+        {
+            return null;
+        }
+
+        ////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////
+        /// GET SITE LINKS FROM CONTEXT
+        ////////////////////////////////////////////////////
+        public List<string> getSiteLinks(String LastSearchString)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ MainForm.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace wib
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.start_button = new System.Windows.Forms.Button();
+            this.text_url = new System.Windows.Forms.TextBox();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.status_textbox = new System.Windows.Forms.RichTextBox();
+            this.radio_nosub = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radio_yessub = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(216, 188);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(99, 24);
+            this.start_button.TabIndex = 1;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text_url
+            // 
+            this.text_url.Location = new System.Drawing.Point(6, 19);
+            this.text_url.Name = "text_url";
+            this.text_url.Size = new System.Drawing.Size(183, 20);
+            this.text_url.TabIndex = 2;
+            // 
+            // stop_button
+            // 
+            this.stop_button.Enabled = false;
+            this.stop_button.Location = new System.Drawing.Point(321, 188);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(97, 24);
+            this.stop_button.TabIndex = 3;
+            this.stop_button.Text = "Stop";
+            this.stop_button.UseVisualStyleBackColor = true;
+            this.stop_button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // status_textbox
+            // 
+            this.status_textbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.status_textbox.Location = new System.Drawing.Point(216, 99);
+            this.status_textbox.Name = "status_textbox";
+            this.status_textbox.ReadOnly = true;
+            this.status_textbox.Size = new System.Drawing.Size(378, 83);
+            this.status_textbox.TabIndex = 0;
+            this.status_textbox.Text = "";
+            // 
+            // radio_nosub
+            // 
+            this.radio_nosub.AutoSize = true;
+            this.radio_nosub.Location = new System.Drawing.Point(71, 21);
+            this.radio_nosub.Name = "radio_nosub";
+            this.radio_nosub.Size = new System.Drawing.Size(39, 17);
+            this.radio_nosub.TabIndex = 10;
+            this.radio_nosub.Text = "No";
+            this.radio_nosub.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(485, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Made by bugfish";
+            // 
+            // radio_yessub
+            // 
+            this.radio_yessub.AutoSize = true;
+            this.radio_yessub.Checked = true;
+            this.radio_yessub.Location = new System.Drawing.Point(18, 21);
+            this.radio_yessub.Name = "radio_yessub";
+            this.radio_yessub.Size = new System.Drawing.Size(43, 17);
+            this.radio_yessub.TabIndex = 13;
+            this.radio_yessub.TabStop = true;
+            this.radio_yessub.Text = "Yes";
+            this.radio_yessub.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(100)))), ((int)(((byte)(0)))));
+            this.groupBox1.Controls.Add(this.radio_yessub);
+            this.groupBox1.Controls.Add(this.radio_nosub);
+            this.groupBox1.Location = new System.Drawing.Point(417, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(177, 51);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search URLs Sublinks";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(327, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 25);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Web Image Downloader 1.0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(100)))), ((int)(((byte)(0)))));
+            this.groupBox3.Controls.Add(this.text_url);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox3.Location = new System.Drawing.Point(216, 41);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(195, 52);
+            this.groupBox3.TabIndex = 21;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(509, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "www.bugfishtm.de";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(100)))), ((int)(((byte)(0)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(606, 227);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.status_textbox);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "WebImageDownloader by Bugfish";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.TextBox text_url;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.RichTextBox status_textbox;
+        private System.Windows.Forms.RadioButton radio_nosub;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radio_yessub;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
